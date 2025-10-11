805ee8e7d9fa94d650e4af870da7ab75f3accd6c
cmdyz123 <1414646277@qq.com>
2025-10-11 11:11:06 +0800

    Add files via upload

---
+++ 前端作业/10-11/js基础.js
@@ -0,0 +1,7 @@
+        let name2 = "wyq-12204";
+        // document.write(name2);
+        // alert(name2)
+        console.log(name2)
+        console.log(typeof(name2))
+        const a = 100
+        
